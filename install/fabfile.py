8f3087a9b26ffc5d6f3e296fc87bde9f5729049f
despiegk <kristof@incubaid.com>
2014-10-28 08:29:40 +0100

    Create fabfile.py

---
+++ install/fabfile.py
@@ -0,0 +1,10 @@
+#fabric install script (not using jumpscale)
+
+#install jumpscale (from code)
+#install influxdb (jpackage)
+#install hekad (jpackage)
+
+def hello():
+    print("Hello world!")
+    
+    
